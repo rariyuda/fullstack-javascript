cddcff967fe39a7d8b6f955bb62502f9ec69c362
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-10 22:03:51 +0700

    array

---
+++ 02 Control Flow/array.js
@@ -0,0 +1,27 @@
+// Declaring array
+const numArr = [1,2,3,4,5];
+console.log(numArr);
+
+const stringArr = ['eat','code','sleep','repeat'];
+console.log(stringArr);
+
+// Access item
+for(let i = 0; i < stringArr.length;i++){
+    console.log(stringArr[i]);
+}
+
+// Nested array
+const nestArr = [0,["jiro","wan"],1,true, 'three']
+console.log(nestArr);
+console.log(nestArr[1][1]);
+
+// Exercise
+const favSingers = ["Judika", "Ariel", "Ari Lasso"]
+console.log(favSingers[0]);
+const favNumbers = [1,5,7,9];
+const mixedArr = ["string",["otherarrray"],123,true]
+console.log(mixedArr[1]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[3]);
+
+// Array operations
