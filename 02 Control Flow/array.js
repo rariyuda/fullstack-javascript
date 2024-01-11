fac011ccf45ef4938ba585cd881f1db205cb3deb
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-11 11:08:22 +0700

    array operation

--- 02 Control Flow/array.js
+++ 02 Control Flow/array.js
@@ -25,3 +25,18 @@ console.log(mixedArr[1][0]);
 console.log(mixedArr[3]);
 
 // Array operations
+const leftArray = ["Lenin", "Karl", "Stalin", "Aleks","Vladimir","Ivankov"];
+const rightArray = ["John", "George", "Hillary", "Abraham","Joe"];
+let peaceArray = leftArray.concat(rightArray);
+
+console.log(leftArray);
+console.log(rightArray);
+console.log(peaceArray);
+
+console.log(peaceArray.slice(3,8));
+console.log(peaceArray.splice(1,4));
+console.log(peaceArray.join(" vs "));
+
+console.log("Joh ->",peaceArray.includes('Joh'));
+console.log("john ->",peaceArray.includes('john'));
+console.log("John ->",peaceArray.includes("John"));
