6bbefa36af353c7c4b549185b1b4649dd34a1d30
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-15 22:55:45 +0700

    timer and interval

--- 06 Date/date.js
+++ 06 Date/date.js
@@ -15,3 +15,26 @@ console.log(`Hour: ${hour}`);
 
 console.log(date.toDateString());
 console.log(date.toISOString());
+console.log(date.toLocaleDateString());
+
+date.setDate(date.getDate() + 1);
+console.log(date);
+
+// setInterval
+// setInterval(
+//   () => console.log(`This function will be executed every 2 seconds.`),
+//   2000
+// );
+
+const IntervalID = setInterval(function () {
+  console.log(`This function is being executed at the 1s interval`);
+}, 1000);
+//stop after 10 seconds
+setTimeout(function () {
+  clearInterval(IntervalID);
+}, 10000);
+
+// setTimeout
+setTimeout(function () {
+  console.log(`This function will be executed after 3 seconds.`);
+}, 3000);
