623f77fbacf84bd7e4b5640eec39f26cc2302b91
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-16 23:50:02 +0700

    template strings

--- Part II/01 Template String/apps.js
+++ Part II/01 Template String/apps.js
@@ -6,3 +6,15 @@ console.log(
 );
 console.log(`This is just another
             random text`);
+
+console.log(`${2 + 2}`);
+
+const firstName = "Kremi";
+const lastName = "Jowo";
+console.log(`Hello, I'm ${firstName} ${lastName}!`);
+
+function info() {
+  return `I'm Kremi and I'm 20 y.o.`;
+}
+
+console.log(`${info()}`);
