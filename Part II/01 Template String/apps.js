2e3b4058a7c6f7bb580c8894ca7a7cb63719caa4
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-16 23:53:19 +0700

    string exercise

--- Part II/01 Template String/apps.js
+++ Part II/01 Template String/apps.js
@@ -9,8 +9,8 @@ console.log(`This is just another
 
 console.log(`${2 + 2}`);
 
-const firstName = "Kremi";
-const lastName = "Jowo";
+var firstName = "Kremi";
+var lastName = "Jowo";
 console.log(`Hello, I'm ${firstName} ${lastName}!`);
 
 function info() {
@@ -18,3 +18,15 @@ function info() {
 }
 
 console.log(`${info()}`);
+
+// Exercise
+console.log(`
+The quick
+brown fox
+jumps over
+the lazy dog`);
+
+firstName = "Romo";
+lastName = "Sastrodjiwo";
+
+console.log(`The legend of the old world is ${firstName} ${lastName}`);
