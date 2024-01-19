1c9421d15b3a625c521d518f3df1f1c3a3467e3a
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-19 22:38:50 +0700

    Exercise

--- Part II - Intermediate/10 For-Each Loop/apps.js
+++ Part II - Intermediate/10 For-Each Loop/apps.js
@@ -1,5 +1,5 @@
 // When using forEach helper, we pass in anonymous callback function,
-// this function gets called one time for every element in the array unconditionally
+// this function gets called one time for every element in the array
 
 //eg.1
 const colors = ["red", "green", "blue", "pink"];
@@ -17,13 +17,21 @@ console.log(words);
 // Exercise
 // 1. Create an array of numbers.
 const numbers = [0, 8, 5];
+
 // 2. Create variable that hold the sum.
 var sum = 0;
+
 // 3. Create the adder function which takes the parameter of number & add that with sum variable.
+// function adder(number) {
+//   sum += number;
+// }
+//OR
 adder = (number) => (sum += number);
+
 // 4. Loop over the array, incrementing the sum variable except this time a function gets referenced in the forEach
 //numbers.forEach((number) => adder(number));
 //OR
 numbers.forEach(adder);
+
 // 5. Print the sum variable
 console.log(sum);
