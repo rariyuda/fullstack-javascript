6660f4e3119d4c16c98e55d7043b6d02a75cc576
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-27 22:54:36 +0700

    accessing dom

---
+++ Part III - DOM/02. Accessing DOM/apps.js
@@ -0,0 +1,13 @@
+// DOM SELECTOR
+// 1. getELementsByTagName
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByTagName("h1").length);
+// 2. getElementByID
+console.log(document.getElementById("main"));
+console.log(document.getElementById("id-1"));
+// 3. getElementsByClassName
+console.log(document.getElementsByClassName("cls"));
+console.log(document.getElementsByClassName("cls-1"));
+// 4. querySelector -> this will be the most used one.
+
+// 5. querySelectorAll
