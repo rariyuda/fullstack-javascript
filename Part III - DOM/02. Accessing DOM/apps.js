c247845db418f79035ac5490b136a21cbbd9d299
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-28 22:37:04 +0700

    exercise

--- Part III - DOM/02. Accessing DOM/apps.js
+++ Part III - DOM/02. Accessing DOM/apps.js
@@ -1,4 +1,5 @@
 // DOM SELECTOR
+/*
 // 1. getELementsByTagName
 console.log(document.getElementsByTagName("h1"));
 console.log(document.getElementsByTagName("h1").length);
@@ -16,3 +17,33 @@ console.log(document.querySelector(".cls")); // will only select the first one.
 // 5. querySelectorAll
 console.log(document.querySelectorAll(".cls")); // willselect all.
 console.log(document.querySelectorAll("li"));
+
+// storing data in variables
+const h1 = document.querySelector("h1");
+console.log(h1);
+const liAll = document.querySelectorAll("li");
+console.log(liAll);
+*/
+
+// Exercise 1:
+// 1. Select element h4 which holds text of "Red" by using getElementsByTagName
+const h4 = document.getElementsByTagName("h4");
+console.log(h4);
+
+// 2. Select div with the class of "green" which holds text (Green) by using getElementsByClassName
+const divGreen = document.getElementsByClassName("green");
+console.log(divGreen);
+
+// 3. Select div with the ID of blue which holds the text "Blue" by using getElementsById
+const divBlue = document.getElementById("blue");
+console.log(divBlue);
+
+// 4. Select div which has the class & Id of "yellow" by using querySelector()
+const classYellow = document.querySelector(".yellow");
+const idYellow = document.querySelector("#yellow");
+console.log(classYellow);
+console.log(idYellow);
+
+// 5. Select all the elements which has the class of "teal" by using querySelectorAll
+const allTeal = document.querySelectorAll(".teal");
+console.log(allTeal);
