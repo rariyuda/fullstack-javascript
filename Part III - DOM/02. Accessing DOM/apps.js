59b7baf766b73dabe7a2bd221c33d08dfc30d419
Reinal Ariyuda <Kjowo123@gmail.com>
2024-01-28 22:07:27 +0700

    query selector

--- Part III - DOM/02. Accessing DOM/apps.js
+++ Part III - DOM/02. Accessing DOM/apps.js
@@ -9,5 +9,10 @@ console.log(document.getElementById("id-1"));
 console.log(document.getElementsByClassName("cls"));
 console.log(document.getElementsByClassName("cls-1"));
 // 4. querySelector -> this will be the most used one.
+console.log(document.querySelector("h2"));
+console.log(document.querySelector("#id-1"));
+console.log(document.querySelector(".cls")); // will only select the first one.
 
 // 5. querySelectorAll
+console.log(document.querySelectorAll(".cls")); // willselect all.
+console.log(document.querySelectorAll("li"));
