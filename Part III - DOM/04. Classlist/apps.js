ca72e962131bed9beeaea8d3176654552895b261
Reinal Ariyuda <Kjowo123@gmail.com>
2024-02-01 21:51:24 +0700

    classlist js

--- Part III - DOM/04. Classlist/apps.js
+++ Part III - DOM/04. Classlist/apps.js
@@ -1,4 +1,15 @@
 // classlist
+const h1 = document.querySelector("h1");
+console.log(h1.classList);
 // add()
+h1.classList.add("greet");
+console.log(h1.classList);
 // remove()
+h1.classList.remove("one");
+console.log(h1.classList);
 // toggle()
+h1.classList.toggle("styles");
+console.log(h1.classList);
+
+h1.classList.toggle("styles");
+console.log(h1.classList);
